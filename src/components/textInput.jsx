bd5e8c6301734bb61d2bbab0838b2503a4d9ecad
DL-Berkey <ruyria@naver.com>
2022-05-09 16:58:49 +0900

    change textInput.jsx, add google fonts

--- src/components/textInput.jsx
+++ src/components/textInput.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "./textInput.css"
 import AspectRatioIcon from '@mui/icons-material/AspectRatio';
 import SaveIcon from '@mui/icons-material/Save';
 
 function TextInput() {
-    console.log('hi');
+    const [text, setText] = useState("");
+    console.log(text);
     return(
-        <form>
-            <input type="text" />
-            <button><AspectRatioIcon /></button>
-            <button><SaveIcon /></button>
+        <form id="textInputForm">
+            <textarea id="textInput" placeholder="언제나 간편하게 작성하세요." value={ text } onChange={ (e) => setText(e.target.value) }></textarea>
+            <button id="expandButton"><AspectRatioIcon color="primary" sx={{ fontSize: 40 }} /></button>
+            <button id="saveButton"><SaveIcon color="primary" sx={{ fontSize: 40 }} /></button>
         </form>
     );
-} 
+}
 
 export default TextInput;
--- src/index.js
+++ src/index.js
@@ -6,15 +6,12 @@ import TextInput from './components/textInput';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 console.log('hi');
 root.render(
-  <React.StrictMode>
-    <div id="main">
-      <div id='content'>
-        <h4>언제든지 간단하게</h4>
-        <TextInput />
-        <h5>Localstorage를 이용하고 있습니다.</h5>
-      </div>
+  <div id="main">
+    <div id='content'>
+      <TextInput />
+      <h5>Localstorage를 이용하고 있습니다.</h5>
     </div>
-  </React.StrictMode>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
