0a3ba5192bf45e49c4675c6f0eac2e836c350e9b
DL-Berkey <ruyria@naver.com>
2022-05-11 19:22:43 +0900

    update textInput.jsx

--- src/components/textInput.jsx
+++ src/components/textInput.jsx
@@ -1,22 +1,20 @@
-import React, { useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import "./textInput.css"
 import AspectRatioIcon from '@mui/icons-material/AspectRatio';
 import SaveIcon from '@mui/icons-material/Save';
 
 function TextInput() {
-    const [text, setText] = useState(""); //이거 삭제하고 다른걸로 만들어야함
+    const [text, setText] = useState("");
     const [textInputSize, setTextInputSize] = useState("35");
 
-    const handleTextInputSizeChanging = (event) => {
-        let textInputSizeValue = (event.target.value.split("\n").length) * 35
+    const textRef = useRef();
+    const handleResizeHeight = useCallback(() => {
+        textRef.current.style.height = textRef.current.scrollHeight + "px"
+    }, []);
 
-        setTextInputSize(textInputSizeValue)
-        setText(event.target.value);
-    }
-
-    return(
+    return (
         <form id="textInputForm">
-            <textarea id="textInput" placeholder="언제나 간편하게 작성하세요." value={ text } onChange={ handleTextInputSizeChanging } style={{ height: textInputSize + "px" }}></textarea>
+            <textarea id="textInput" placeholder="언제나 간편하게 작성하세요." cols="17" ref={ textRef } onInput={ handleResizeHeight } style={{ height: textInputSize + "px" }}></textarea>
             <button id="expandButton"><AspectRatioIcon color="primary" /></button>
             <button id="saveButton"><SaveIcon color="primary" /></button>
         </form>
