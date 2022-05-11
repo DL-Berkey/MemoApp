3fd35aba73bafcf0662dadbff2127389bdbb8ac8
DL-Berkey <ruyria@naver.com>
2022-05-11 23:49:21 +0900

    update textInput.jsx

--- src/components/textInput.jsx
+++ src/components/textInput.jsx
@@ -5,17 +5,28 @@ import SaveIcon from '@mui/icons-material/Save';
 
 function TextInput() {
     const [text, setText] = useState("");
-    const [textInputSize, setTextInputSize] = useState("35");
+    const [textInputSize, setTextInputSize] = useState(["14", "35"]); //[width, height]
+    const [expandBoolean, setExpandBoolean] = useState(false);
+
+    const textInputStyle = {
+        width: textInputSize[0] + "rem",
+        height: textInputSize[1] + "px",
+        transition: "0.5s",
+    };
 
     const textRef = useRef();
     const handleResizeHeight = useCallback(() => {
         textRef.current.style.height = textRef.current.scrollHeight + "px"
     }, []);
 
+    const expand = function (event) {
+        setTextInputSize([24,100])
+    }
+
     return (
         <form id="textInputForm">
-            <textarea id="textInput" placeholder="언제나 간편하게 작성하세요." cols="17" ref={ textRef } onInput={ handleResizeHeight } style={{ height: textInputSize + "px" }}></textarea>
-            <button id="expandButton"><AspectRatioIcon color="primary" /></button>
+            <textarea id="textInput" placeholder="언제나 간편하게 작성하세요." ref={ textRef } onInput={ handleResizeHeight } style={ textInputStyle }></textarea>
+            <button type="button" id="expandButton" onClick={ expand }><AspectRatioIcon color="primary" /></button>
             <button id="saveButton"><SaveIcon color="primary" /></button>
         </form>
     );
