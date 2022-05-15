2e5ef7ec7ffe1ce0e4aae7f01c02d33a5a7f6b95
DL-Berkey <ruyria@naver.com>
2022-05-15 22:26:06 +0900

    before refactoring

--- src/components/textInput.jsx
+++ src/components/textInput.jsx
@@ -3,54 +3,60 @@ import "./textInput.css"
 import AspectRatioIcon from '@mui/icons-material/AspectRatio';
 import SaveIcon from '@mui/icons-material/Save';
 
+const countChar = function(text, char) {
+    let count = [...text].filter((item) => item === char).length + 1;
+
+    return count;
+}
+
+const resizeByText = function(text, scale) {
+    let value = ""
+
+    if(scale === "up") {
+        console.log("up")
+        value = text + "\n";
+        console.log([...text])
+        //+ "\n"
+    }
+
+    if(scale === "down" && text[text.length - 1] === "\n") {
+        console.log("down")
+        value = [...text].splice(0, text.length - 1).join("");
+
+    }
+    const enterCount = countChar(value, "\n") * 35 || 35;
+    console.log()
+    setSize([size[0], enterCount]);
+    console.log(value);
+    return value;
+}
+
+const handleInputEvent = function(event) {
+    const key = event.key;     
+    let text = event.target.value;
+
+    if (text.length !== 0 && text.length % 17 === 0 && text[text.length - 1] !== "\n" && key === "") {
+        resizeByText(text, "up");
+    }
+
+    if (key === "Enter") {
+        text = resizeByText(text, "up");
+    }
+
+    if(key === "Backspace") {
+        text = resizeByText(text, "down");
+    }
+
+    textInputRef.current.value = text;
+    console.log([...text]);
+}
+
 function TextInput() {
     const [size, setSize] = useState(["14", "35"]); //[width, height]
+    const textInputRef = useRef();
     const style = {
         width: size[0] + "rem",
         height: size[1] + "px",
-    }
-
-    const countChar = function(text, char) {
-        let count = [...text].filter((item) => item === char).length
-
-        if(count === 1) {
-            count = 2;
-        }
-        return count;
-    }
-
-    const resizeByText = function (text, scale) {
-        if(scale === "up") {
-            text = [...text, "\n"].join("")
-        }
-
-        if(scale === "down" && text[text.length - 1] === "\n") {
-            text = [...text].splice(0, text.length - 1).join("");
-
-        }
-        console.log([...text]);
-        const enterCount = countChar(text, "\n") * 35;
-        setSize([size[0], enterCount]);
-    }
-
-    const handleResize = function (event) {
-        let text = event.target.value;
-
-        if (text.length % 18 === 0 && text[text.length - 1] !== "\n") {
-            resizeByText(text, "up");
-        }
-    }
-
-    const handleKeyInput = function (event) {
-        const key = event.key;
-        let text = event.target.value;
-        if (key === "Enter") {
-            resizeByText(text, "up");
-        }
-
-        if(key === "Backspace") {
-            resizeByText(text, "down")
-        }
     }
 
     return (
@@ -58,10 +64,10 @@ function TextInput() {
             <textarea
                 id="textInput"
                 placeholder="언제나 간편하게 작성하세요."
-                cols="3"
+                ref={ textInputRef }
                 style={ style }
-                onChange={ handleResize }
-                onKeyDown={ handleKeyInput }
+                // value={ text }
+                onKeyDown={ handleInputEvent }
             >
             </textarea>
             <button type="button" id="expandButton" ><AspectRatioIcon color="primary" /></button>
